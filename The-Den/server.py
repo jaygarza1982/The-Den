0f2e84b3bfe684d16cdd6b22fac70847265300dd
Jake <jaygarza1982@gmail.com>
2019-12-23 13:35:04 -0500

    server class now gets username with get_username method

--- The-Den/server.py
+++ The-Den/server.py
@@ -98,32 +98,37 @@ class server:
 
         @app.route('/settings')
         def indexSettings():
-            username = session[request.cookies.get('logintoken')]
-            user = users.user(username, '')
-            return render_template_string(open('public/templates/settings-template.html', 'r').read(), regex_filters=user.get_regex_filters())
+            username = get_username(self, request)
+            if username != None:
+                user = users.user(username, '')
+                return render_template_string(open('public/templates/settings-template.html', 'r').read(), regex_filters=user.get_regex_filters())
+            return make_response(redirect('/'))
 
         @app.route('/comment', methods=['POST'])
         def comment():
             #Get the username of logged in client
-            username = session[request.cookies.get('logintoken')] #request.form['user']
+            username = get_username(self, request)
 
-            comment = request.form['comment']
-            if comment.strip() != '':
-                user = users.user(username, '')
-                user.make_comment(commentRW, request.form['user'], request.form['postID'], request.form['comment'])
-                # user.make_comment(request.form['user'], request.form['postID'], request.form['comment'])
-                return 'Success!'
-            else:
-                #Not modified
-                abort(304)
+            if username != None:
+                comment = request.form['comment']
+                if comment.strip() != '':
+                    user = users.user(username, '')
+                    user.make_comment(commentRW, request.form['user'], request.form['postID'], request.form['comment'])
+                    return 'Success!'
+                else:
+                    #Not modified
+                    abort(304)
+            return make_response(redirect('/'))
 
         @app.route('/post', methods=['POST'])
         def post():
-            username = session[request.cookies.get('logintoken')]
-            user = users.user(username, '')
-            user.make_post(request.form['caption'])
-            
-            return 'Success!'
+            username = get_username(self, request)
+            if username != None:
+                user = users.user(username, '')
+                user.make_post(request.form['caption'])
+                
+                return 'Success!'
+            return make_response(redirect('/'))
 
         @app.route('/login', methods=['POST'])
         def login():
@@ -146,29 +151,30 @@ class server:
 
         @app.route('/user-query')
         def indexUserQuery():
-            current_user = users.user(session[request.cookies.get('logintoken')], '')
-            following = current_user.get_following()
+            username = get_username(self, request)
+            if username != None:
+                current_user = users.user(username, '')
+                following = current_user.get_following()
 
-            following_status = [{}]
+                following_status = [{}]
 
-            username_prefix = request.args.get('prefix')
-            possible_users = ''
-            if (username_prefix.strip() != ''):
-                usernames = os.listdir('./users')
-                
-                for user in usernames:
-                    if (user.startswith(username_prefix)):
-                        following_status[-1]['username'] = user
-                        following_status[-1]['following'] = user in following
-                        # following_status[user] = user in following
-                        following_status.append({})
+                username_prefix = request.args.get('prefix')
+                possible_users = ''
+                if (username_prefix.strip() != ''):
+                    usernames = os.listdir('./users')
+                    
+                    for user in usernames:
+                        if (user.startswith(username_prefix)):
+                            following_status[-1]['username'] = user
+                            following_status[-1]['following'] = user in following
+                            following_status.append({})
 
-            return jsonify(following_status[:-1])
-            # return possible_users[:-1]
+                return jsonify(following_status[:-1])
+            return make_response(redirect('/'))
 
         @app.route('/follow', methods=['POST'])
         def indexFollow():
-            current_user = session[request.cookies.get('logintoken')]
+            current_user = get_username(self, request)
             username_to_follow = request.form['username']
             
             user = users.user(current_user, '')
@@ -178,7 +184,7 @@ class server:
 
         @app.route('/unfollow', methods=['POST'])
         def indexUnfollow():
-            current_username = session[request.cookies.get('logintoken')]
+            current_username = get_username(self, request)
             user_to_unfollow = request.form['username']
 
             current_user = users.user(current_username, '')
@@ -188,7 +194,7 @@ class server:
 
         @app.route('/regex', methods=['POST'])
         def indexRegex():
-            current_username = session[request.cookies.get('logintoken')]
+            current_username = get_username(self, request)
             user = users.user(current_username, '')
             user.save_regex_filter(request.form['regex-filters'])
             return 'Sent.'
